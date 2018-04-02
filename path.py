6d9365b58ec4da79772bcbf62726db4f5dd5da51
HHEjie123 <35299864+HHEjie123@users.noreply.github.com>
2018-04-02 07:20:11 +0000

    Update path.py

--- path.py
+++ path.py
@@ -1,1 +1,8 @@
 /home/nvidia/hjnew/py-faster-rcnn/data/demo
+Demo for data/demo/4.jpg
+4.jpg
+Detection took 0.911s for 300 object proposals
+kkkk red zhuan   869.548   458.07297   985.23987   584.81494
+kkkk wangqiu   1239.9375   440.86002   1386.9868   595.9096
+{'A': {'right': 'wangqiu', 'mid': 'red zhuan', 'left': 'None'}, 'C': {'right': 'wangqiu', 'mid': 'red zhuan', 'left': 'None'}, 'B': {'right': 'wangqiu', 'mid': 'red zhuan', 'left': 'blue zhuan'}, 'D': {'right': 'wangqiu', 'mid': 'red zhuan', 'left': 'None'}}
+Time: 8.3848669529
