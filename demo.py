34519928ce59ecef2c5eafb346ee5020c488a45c
HHEjie123 <35299864+HHEjie123@users.noreply.github.com>
2018-04-02 07:15:08 +0000

    Update demo.py

--- demo.py
+++ demo.py
@@ -24,47 +24,50 @@ import scipy.io as sio
 import caffe, os, sys, cv2
 import argparse
 import time
-#from forcaption import RobotCarHandle
 
-#CLASSES = ('__background__',
+# from forcaption import RobotCarHandle
+
+# CLASSES = ('__background__',
 #            'greenzhuan', 'blue zhuan', 'xuehua', 'green zhuan',
 #                                 'xuebi', 'red zhuan', 'red zhuan', 'shuangwaiwai', 'apple',
 #                                 'apple','gangsiqiu','wangqiu')
 CLASSES = ('__background__',
-            'greenzhuan', 'blue zhuan', 'yumaoqiu', 'xuebi',
-                                 'xuehua', 'red zhuan', 'yellow zhuan',
-                                 'apple','gangsiqiu','wangqiu')
+           'greenzhuan', 'blue zhuan', 'yumaoqiu', 'xuebi',
+           'xuehua', 'red zhuan', 'yellow zhuan',
+           'apple', 'gangsiqiu', 'wangqiu')
 NETS = {'vgg16': ('VGG16',
                   'VGG16_faster_rcnn_final.caffemodel'),
         'zf': ('ZF',
-                  'hjZF_faster_rcnn_final.caffemodel')}
+               'hjZF_faster_rcnn_final.caffemodel')}
 
-def zuobiao(x1,x2,x3,x4):
-    return x1,x2,x3,x4
 
-def vis_detections(im, class_name, dets,ax, thresh=0.5):
+def zuobiao(x1, x2, x3, x4):
+    return x1, x2, x3, x4
+
+
+def vis_detections(im, class_name, dets, ax, thresh=0.5):
     """Draw detected bounding boxes."""
     inds = np.where(dets[:, -1] >= thresh)[0]
-    x1=0 
-    x2=0
-    x3=0 
-    x4=0 
+    x1 = 0
+    x2 = 0
+    x3 = 0
+    x4 = 0
     if len(inds) == 0:
-        return x1,x2,x3,x4,class_name
-   
-    #im = im[:, :, (2, 1, 0)]
-    #fig, ax = plt.subplots(figsize=(12, 12))
-    #ax.imshow(im, aspect='equal')
+        return x1, x2, x3, x4, class_name
+
+    # im = im[:, :, (2, 1, 0)]
+    # fig, ax = plt.subplots(figsize=(12, 12))
+    # ax.imshow(im, aspect='equal')
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
-        x1,x2,x3,x4=zuobiao(bbox[0],bbox[1],bbox[2],bbox[3])
+        x1, x2, x3, x4 = zuobiao(bbox[0], bbox[1], bbox[2], bbox[3])
         ax.add_patch(
             plt.Rectangle((bbox[0], bbox[1]),
                           bbox[2] - bbox[0],
                           bbox[3] - bbox[1], fill=False,
                           edgecolor='red', linewidth=3.5)
-            )
+        )
         ax.text(bbox[0], bbox[1] - 2,
                 '{:s} {:.3f}'.format(class_name, score),
                 bbox=dict(facecolor='blue', alpha=0.5),
@@ -73,20 +76,14 @@ def vis_detections(im, class_name, dets,ax, thresh=0.5):
     ax.set_title(('{} detections with '
                   'p({} | box) >= {:.1f}').format(class_name, class_name,
                                                   thresh),
-                  fontsize=14)
-    #plt.axis('off')
-    #plt.tight_layout()
-    #plt.draw()
-    print 'kkkk',class_name,' ',x1,' ',x2,' ',x3,' ',x4
-    #if class_name != None :    
-    return x1,x2,x3,x4,class_name
+                 fontsize=14)
+    # plt.axis('off')
+    # plt.tight_layout()
+    # plt.draw()
+    print 'kkkk', class_name, ' ', x1, ' ', x2, ' ', x3, ' ', x4
+    # if class_name != None :
+    return x1, x2, x3, x4, class_name
 
-     
-   
-      
-   
-
-    
 
 def demo(net, image_name):
     """Detect object classes in an image using pre-computed object proposals."""
@@ -102,8 +99,6 @@ def demo(net, image_name):
     print ('Detection took {:.3f}s for '
            '{:d} object proposals').format(timer.total_time, boxes.shape[0])
 
-  
-
     # Visualize detections for each class
     CONF_THRESH = 0.6
     NMS_THRESH = 0.3
@@ -111,26 +106,30 @@ def demo(net, image_name):
     fig, ax = plt.subplots(figsize=(12, 12))
     ax.imshow(im, aspect='equal')
     for cls_ind, cls in enumerate(CLASSES[1:]):
-        cls_ind += 1 # because we skipped background
-        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_ind += 1  # because we skipped background
+        cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
         cls_scores = scores[:, cls_ind]
         dets = np.hstack((cls_boxes,
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
-     
-        x1,x2,x3,x4,class_name=vis_detections(im, cls, dets,ax, thresh=CONF_THRESH)
-        if(x1 != 0 and x1<1600):
-            if x1<600:
-                print>>f,'left',class_name
-            elif x1<1000:
-                print>>f,'mid',class_name
+
+        x1, x2, x3, x4, class_name = vis_detections(im, cls, dets, ax, thresh=CONF_THRESH)
+        if (x1 != 0 and x1 < 1600):
+            if x1 < 600:
+                print>> f, 'left', class_name
+                dictf[image_name+'left']=class_name
+            elif x1 < 1000:
+                print>> f, 'mid', class_name
+                dictf[image_name+'mid'] =  class_name
             else:
-                print>>f,'right',class_name
+                print>> f, 'right', class_name
+                dictf[image_name+'right'] =  class_name
     plt.axis('off')
     plt.tight_layout()
     plt.draw()
-    
+
+
 def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='Faster R-CNN demo')
@@ -146,8 +145,9 @@ def parse_args():
 
     return args
 
+
 if __name__ == '__main__':
-    t=time.time()
+    t = time.time()
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
 
     args = parse_args()
@@ -174,23 +174,41 @@ if __name__ == '__main__':
     # Warmup on a dummy image
     im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
     for i in xrange(2):
-        _, _= im_detect(net, im)
-    
-    im_names = ['1.jpg','2.jpg','3.jpg','4.jpg']
-    f=open('out.txt','w')
-    #filepath = time.strftime('%Y-%m-%d-%H-%M-%S')
-    filepath="ssss"
-   # vision = RobotCarHandle(filepath)   
-   # vision.open_camera()
-   # vision.camera_takephoto(29);
-   # vision.close_camera();
+        _, _ = im_detect(net, im)
+
+    im_names = ['1.jpg', '2.jpg', '3.jpg', '4.jpg']
+    f = open('out.txt', 'w')
+    # filepath = time.strftime('%Y-%m-%d-%H-%M-%S')
+    filepath = "ssss"
+    # vision = RobotCarHandle(filepath)
+    # vision.open_camera()
+    # vision.camera_takephoto(29);
+    # vision.close_camera();
+    dictf={}
     for im_name in im_names:
         print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
         print 'Demo for data/demo/{}'.format(im_name)
-        print>>f,im_name
-        demo(net, im_name) 
-        
+        print>> f, im_name
+        print(im_name)
+        demo(net, im_name)
+
+    itemlistp={}
+    itemlist=('1.jpgleft','1.jpgmid','1.jpgright','2.jpgleft','2.jpgmid','2.jpgright',
+              '3.jpgleft','3.jpgmid','3.jpgright','4.jpgleft','4.jpgmid','4.jpgright')
+    for klp in range(len(itemlist)):
+        if itemlist[klp] in dictf:
+            itemlistp[itemlist[klp]]=dictf[itemlist[klp]]
+        else:
+            itemlistp[itemlist[klp]]='None'
+    item_list={"A": {"left": itemlistp['1.jpgleft'], "mid": itemlistp['1.jpgmid'], "right": itemlistp['1.jpgright']},
+               "B": {"left": itemlistp['2.jpgleft'], "mid": itemlistp['2.jpgmid'], "right": itemlistp['2.jpgright']},
+               "C": {"left": itemlistp['3.jpgleft'], "mid": itemlistp['3.jpgmid'], "right": itemlistp['3.jpgright']},
+               "D": {"left": itemlistp['4.jpgleft'], "mid": itemlistp['4.jpgmid'], "right": itemlistp['4.jpgright']}}
+
+    print(item_list)
+
+
     f.close()
-    print "Time:",time.time()-t
+    print "Time:", time.time() - t
     plt.show()
-   
+
