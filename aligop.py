43af956f22c12ecfd0759a98d73627f2053d546b
HHEjie123 <35299864+HHEjie123@users.noreply.github.com>
2018-04-03 09:16:21 -0500

    Update aligop.py

--- aligop.py
+++ aligop.py
@@ -61,84 +61,83 @@ def huojia(picture,pix,r,g,b):
 
 # Streaming loop
 def picture():
-    pipeline = rs.pipeline()
-    # Create a config and configure the pipeline to stream
-    #  different resolutions of color and depth streams
-    config = rs.config()
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-    profile = pipeline.start(config)
-    # Getting the depth sensor's depth scale (see rs-align example for explanation)
-    depth_sensor = profile.get_device().first_depth_sensor()
-    depth_scale = depth_sensor.get_depth_scale()
-    print "Depth Scale is: ", depth_scale
+	pipeline = rs.pipeline()
 
-    # We will be removing the background of objects more than
-    #  clipping_distance_in_meters meters away
-    clipping_distance_in_meters = 0.5  # 1 meter
-    clipping_distance = clipping_distance_in_meters / depth_scale
+	#Create a config and configure the pipeline to stream
+	#  different resolutions of color and depth streams
+	config = rs.config()
+	config.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 30)
+	config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
-    # Create an align object
-    # rs.align allows us to perform alignment of depth frames to others frames
-    # The "align_to" is the stream type to which we plan to align depth frames.
-    align_to = rs.stream.color
-    align = rs.align(align_to)
-    print('  start  ')
-    """
-    while True:
-        # Get frameset of color and depth
+	# Start streaming
+	profile = pipeline.start(config)
 
-        frames = pipeline.wait_for_frames()
-        # frames.get_depth_frame() is a 640x360 depth image
+	# Getting the depth sensor's depth scale (see rs-align example for explanation)
+	depth_sensor = profile.get_device().first_depth_sensor()
+	depth_scale = depth_sensor.get_depth_scale()
+	print "Depth Scale is: " , depth_scale
 
-        # Align the depth frame to color frame
-        aligned_frames = align.process(frames)
+	# We will be removing the background of objects more than
+	#  clipping_distance_in_meters meters away
+	clipping_distance_in_meters = 1 #1 meter
+	clipping_distance = clipping_distance_in_meters / depth_scale
 
-        # Get aligned frames
-        aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
-        color_frame = aligned_frames.get_color_frame()
+	# Create an align object
+	# rs.align allows us to perform alignment of depth frames to others frames
+	# The "align_to" is the stream type to which we plan to align depth frames.
+	align_to = rs.stream.color
+	align = rs.align(align_to)
 
-        # Validate that both frames are valid
-        if not aligned_depth_frame or not color_frame:
-            continue
-
-        depth_image = np.asanyarray(aligned_depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-
-        # Remove background - Set pixels further than clipping_distance to grey
-        grey_color = 0
-        depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
-        bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
-        # Render images
-        #depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.09), cv2.COLORMAP_JET)
-        #images = np.hstack((bg_removed, depth_colormap))
-        cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('Align Example', bg_removed)
-        key = cv2.waitKey()
-        #print key
-        if key ==ord('1'):
-            cv2.imwrite('picture/%d.jpg'%(1),bg_removed)
-            cv2.imwrite('picture/color_%d.jpg' % (1),color_image)
-        if key ==ord('2'):
-            cv2.imwrite('picture/_%d.jpg'%(2),bg_removed)
-            cv2.imwrite('picture/color_%d.jpg' % (2), color_image)
-        if key ==ord('3'):
-            cv2.imwrite('picture/_%d.jpg'%(3),bg_removed)
-            cv2.imwrite('picture/color_%d.jpg' % (3), color_image)
-        if key == ord('4'):
-            cv2.imwrite('picture/%d.jpg' % (4), bg_removed)
-            cv2.imwrite('picture/color_%d.jpg' % (4), color_image)
-        if key == ord('5'):
-            cv2.imwrite('picture/%d.jpg' % (5), bg_removed)
-            cv2.imwrite('picture/color_%d.jpg' % (5), color_image)
-        if key == ord('6'):
-            cv2.imwrite('picture/%d.jpg' % (6), bg_removed)
-            cv2.imwrite('picture/color_%d.jpg' % (6), color_image)
-        if key == ord('s'):
-            print('kjkk')
-            pipeline.stop()
-            break
-    """
+        print('  start  ')
+	
+	while True:
+	# Get frameset of color and depth
+		frames = pipeline.wait_for_frames()
+		# frames.get_depth_frame() is a 640x360 depth image
+		# Align the depth frame to color frame
+                aligned_frames = align.proccess(frames)
+		# Get aligned frames
+		aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+		color_frame = aligned_frames.get_color_frame()
+		# Validate that both frames are valid
+		if not aligned_depth_frame or not color_frame:
+		    continue
+		depth_image = np.asanyarray(aligned_depth_frame.get_data())
+		color_image = np.asanyarray(color_frame.get_data())
+		# Remove background - Set pixels further than clipping_distance to grey
+		grey_color = 0
+		depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+		bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+		# Render images
+		#depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.09), cv2.COLORMAP_JET)
+		#images = np.hstack((bg_removed, depth_colormap))
+		cv2.namedWindow('Align Example', cv2.WINDOW_AUTOSIZE)
+		cv2.imshow('Align Example', bg_removed)
+		key = cv2.waitKey(110)
+		#print key
+		if key ==ord('1'):
+		    cv2.imwrite('picture/%d.jpg'%(1),bg_removed)
+		    cv2.imwrite('picture/color_%d.jpg' % (1),color_image)
+		if key ==ord('2'):
+		    cv2.imwrite('picture/%d.jpg'%(2),bg_removed)
+		    cv2.imwrite('picture/color_%d.jpg' % (2), color_image)
+		if key ==ord('3'):
+		    cv2.imwrite('picture/%d.jpg'%(3),bg_removed)
+		    cv2.imwrite('picture/color_%d.jpg' % (3), color_image)
+		if key == ord('4'):
+		    cv2.imwrite('picture/%d.jpg' % (4), bg_removed)
+		    cv2.imwrite('picture/color_%d.jpg' % (4), color_image)
+		if key == ord('5'):
+		    cv2.imwrite('picture/%d.jpg' % (5), bg_removed)
+		    cv2.imwrite('picture/color_%d.jpg' % (5), color_image)
+		if key == ord('6'):
+		    cv2.imwrite('picture/%d.jpg' % (6), bg_removed)
+		    cv2.imwrite('picture/color_%d.jpg' % (6), color_image)
+		if key == ord('s'):
+		    print('kjkk')
+		    pipeline.stop()
+		    break
+	"""
 
 
         # Get frameset of color and depth
@@ -206,7 +205,7 @@ def picture():
     time.sleep(5)
     cv2.destroyAllWindows()
     pipeline.stop()
-
+	"""
 
 def huop():
     #alist1=[]
