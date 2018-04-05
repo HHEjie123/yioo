ac6658f3432430656518edf125993ae3cc073353
HHEjie123 <35299864+HHEjie123@users.noreply.github.com>
2018-04-05 06:23:13 +0000

    Create peizhi.py

---
+++ peizhi.py
@@ -0,0 +1,21 @@
+pip install protobuf
+sudo apt-get install python-pip
+sudo apt-get install python-skimage
+sudo apt-get install the python-dev
+sudo apt-get python-matplotlib
+sudo apt-get install python-scipy
+sudo apt-get install python-numpy
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler 
+
+sudo apt-get update
+
+
+sudo apt-get install --no-install-recommends libboost-all-dev
+
+sudo apt-get install libopenblas-dev liblapack-dev libatlas-base-dev
+
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+
+sudo apt-get install git cmake build-essential
+gedit ~/.bashrc
+export PYTHONPATH=/home/caffe-master/python/:/home/caffe-master/python/caffe/
